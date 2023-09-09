f1e57a08af0c060c6a35003648a203d55563e940
lamiaa <lamiaa.sq@gmail.com>
2023-09-09 16:41:35 +0300

    Yup validation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Form, Input } from 'informed'
+import { Debug, Form, Input } from 'informed'
+import * as yup from 'yup'
 
+const schema = yup.object({
+  email : yup.string().email('Email Must be a Valid Email Address'),
+  password: yup.string().min(8,'Insert a Minimum of 8  Characters').max(30,'You Reached The Maximum Password Characters')
+});
 const App = () => {
   return (
     <div>
-      <Form onSubmit={(formState)=> console.log(formState.values)}>
-        <Input label='Email' name='email' type='email' placeholder='example@.com'/>
-        <Input label='Password' name='password' type='password' placeholder='********'/>
+      <Form onSubmit={({values})=> console.log(values)} yupSchema={schema} >
+        <Input label='Email' name='email' type='email' placeholder='example@.com' required/>
+        <Input label='Password' name='password' type='password' placeholder='********' required/>
         <button type='submit'>Submit</button>
+        {/* <Debug/> */}
       </Form>
     </div>
   )
