83035d0a91064719f50b0efce11e879189335781
lamiaa <lamiaa.sq@gmail.com>
2023-09-11 22:38:27 +0300

    remove clg

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -13,7 +13,6 @@ const useDarkMode = () => {
       window.document.documentElement.classList.remove(theme ==='light'? 'dark': 'light');
       window.document.documentElement.classList.add(theme);
       localStorage.setItem('theme', theme)
-      console.log('theme: ', theme)
   },[theme,setTheme])
   return [theme, setTheme];
 }
