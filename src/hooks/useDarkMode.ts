6954970a30edad3aa2c4d2f42d06974e0fef32a3
lamiaa <lamiaa.sq@gmail.com>
2023-09-11 22:38:04 +0300

    add support for  prefered OS mode when intially opening the app

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
 const useDarkMode = () => {
-  const [theme, setTheme] = useState<'light' |' dark'>('light');
-  window.localStorage.setItem('theme', theme);
+  const [theme, setTheme] = useState <'light' | 'dark'>('light');
 
   useEffect(()=>{
-    window.document.documentElement.classList.remove(theme ==='light'? 'dark': 'light');
-    window.document.documentElement.classList.add(theme);
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setTheme('dark');
+    }
+  },[])
+
+  useEffect(()=>{
+      window.document.documentElement.classList.remove(theme ==='light'? 'dark': 'light');
+      window.document.documentElement.classList.add(theme);
+      localStorage.setItem('theme', theme)
+      console.log('theme: ', theme)
   },[theme,setTheme])
   return [theme, setTheme];
 }
