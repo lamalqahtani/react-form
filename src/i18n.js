0a96054f6b06ccc69e43e779fc170536a51d5fae
lamiaa <lamiaa.sq@gmail.com>
2023-09-27 22:25:43 +0300

    add language detector to store seleted language in the local storage and use it when refreshing the page

--- src/i18n.js
+++ src/i18n.js
@@ -1,28 +1,31 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(initReactI18next).init({
-    fallbackLng: 'en',
-    lng:'ar',
-    debug: true,
-    resources:{
-        en: {
-            translation: {
-              login: 'Login',
-              email: 'Email',
-              password: 'Password',
-              submit: 'Submit'
-            }
-          },
-          ar: {
-            translation: {
-              "login": 'تسجيل الدخول',
-              "email": 'البريد الالكتروني',
-              "password": 'كلمة المرور',
-              submit: 'تسليم'
-            }
-          }
-    }
+i18n.use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        fallbackLng: 'en',
+        // lng:'ar',
+        debug: true,
+        resources:{
+            en: {
+                translation: {
+                  login: 'Login',
+                  email: 'Email',
+                  password: 'Password',
+                  submit: 'Submit'
+                }
+              },
+              ar: {
+                translation: {
+                  "login": 'تسجيل الدخول',
+                  "email": 'البريد الالكتروني',
+                  "password": 'كلمة المرور',
+                  submit: 'تسليم'
+                }
+              }
+        }
 });
 
 
